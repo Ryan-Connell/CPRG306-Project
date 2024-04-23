50c733fb80946d45c53f9d4a1e20a6aa61b4cb88
Greg Pilkington <greg.j.pilkington@gmail.com>
2024-04-23 10:54:45 -0600

    this is America
    Don't catch you slippin' now

--- guess-the-gathering/app/page.js
+++ guess-the-gathering/app/page.js
@@ -75,9 +75,17 @@ export default function Page() {
                     // name blur
                     <div className="absolute h-6 w-40 name-blur inset-y-1 inset-x-42 rounded-xl"></div>
                   )}
+                  {selectedGameType === "Name" && (
+                    //  rule text type blur
+                    <div className="absolute rule-text-blur inset-y-12 inset-x-42 rounded-xl"></div>
+                  )}
                   {selectedGameType === "Set" && (
                     //  set code blur
                     <div className="absolute h-6 set-code-blur inset-y-12 inset-x-42 rounded-xl"></div>
+                  )}
+                  {selectedGameType === "Set" && (
+                    //  set code blur
+                    <div className="absolute h-6 set-symbol-blur inset-y-12 inset-x-44 rounded-xl"></div>
                   )}
                   {selectedGameType === "Mana Value" && (
                     //  mana value blur
@@ -89,55 +97,72 @@ export default function Page() {
                   )}
                   {selectedGameType === "Power/Toughness" && (
                     //  power/toughness blur
-                    <div className="absolute h-6 pt-blur inset-y-12 inset-x-44 rounded-xl"></div>
+                    <div className="absolute h-8 pt-blur inset-y-12 inset-x-44 rounded-xl"></div>
                   )}
                 </div>
               </div>
             </div>
-            <div className="flex custom-width justify-center mt-16">
-              {selectedGameType === "Name" && (
-                <CardName card={card} handleGetCard={handleGetCard} />
-              )}
-              {selectedGameType === "Set" && card && (
-                <SetName setCode={card.setName} handleGetSet={handleGetCard} />
-              )}
-              {selectedGameType === "Power/Toughness" && (
-                <PowTough card={card} handleGetCard={handleGetCard} />
-              )}
-              {selectedGameType === "Mana Value" && (
-                <ManaValue
-                  manaCost={card ? card.manaCost : ""}
-                  selectedGameType={selectedGameType}
-                  loadNewCard={handleGetCard}
-                />
-              )}
-              {selectedGameType === "Type" && (
-                <Type card={card} handleGetCard={handleGetCard} />
-              )}
+            <div className="flex flex-col justify-center w-full items-center">
+              <div className="flex custom-width justify-center mt-10">
+                {selectedGameType === "Name" && (
+                  <CardName card={card} handleGetCard={handleGetCard} />
+                )}
+                {selectedGameType === "Set" && card && (
+                  <SetName
+                    setCode={card.setName}
+                    handleGetSet={handleGetCard}
+                  />
+                )}
+                {selectedGameType === "Power/Toughness" && (
+                  <PowTough card={card} handleGetCard={handleGetCard} />
+                )}
+                {selectedGameType === "Mana Value" && (
+                  <ManaValue
+                    manaCost={card ? card.manaCost : ""}
+                    selectedGameType={selectedGameType}
+                    loadNewCard={handleGetCard}
+                  />
+                )}
+                {selectedGameType === "Type" && (
+                  <Type card={card} handleGetCard={handleGetCard} />
+                )}
+              </div>
+              <div className="flex justify-center items-end mt-8">
+                <div className="mint-text mb-2">
+                  <h2 className="text-xl font-semibold">Rules</h2>
+                  <div className="mt-2 text-sm">
+                    <p className="spacing">
+                      Name: Hangman-style guessing game where you guess the name
+                      of the card. You have five chances. Green means the right
+                      letter guessed, red means wrong.
+                    </p>
+                    <p className="spacing">
+                      Set: Same as Name game, but for guessing the set name of
+                      the card.
+                    </p>
+                    <p className="spacing">
+                      Mana Value: Guess the mana value by clicking on the colors
+                      of the mana. The game will provide hints to guess higher
+                      or lower.
+                    </p>
+                    <p className="spacing">
+                      Type: Guess the card type from a dropdown menu. Note:
+                      There may be errors with legendary cards.
+                    </p>
+                    <p className="spacing">
+                      Power/Toughness: Guess a card's power and toughness with
+                      hints to guess higher or lower.
+                    </p>
+                  </div>
+                  <h2 className="text-xl font-semibold mt-4">Credits</h2>
+                  <p className="mt-2 text-sm">
+                    {" "}
+                    Made by: Dawson Gall, Greg Pilkington, Ryan Connell
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="absolute bottom-0 mb-8 mx-auto w-full text-center text-white">
-          <h2 className="text-lg font-semibold">Rules</h2>
-          <p className="mt-2 text-sm">
-            {" "}
-            Name: Hangman-style guessing game where you guess the name of the
-            card. You have five chances. Green means the right letter guessed,
-            red means wrong. <br />
-            Set: Same as Name game, but for guessing the set name of the card.{" "}
-            <br />
-            Mana Value: Guess the mana value by clicking on the colors of the
-            mana. The game will provide hints to guess higher or lower. <br />
-            Type: Guess the card type from a dropdown menu. Note: There may be
-            errors with legendary cards. <br />
-            Power/Toughness: Guess a card's power and toughness with hints to
-            guess higher or lower.
-          </p>
-          <h2 className="text-lg font-semibold mt-4">Credits</h2>
-          <p className="mt-2 text-sm">
-            {" "}
-            Made by: Dawson Gall, Greg Pilkington, Ryan Connell
-          </p>
         </div>
       </div>
     </main>
